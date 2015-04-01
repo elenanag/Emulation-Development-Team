162bd00fbff2e7e46740c2b9c55d7f9b0807f80f
Elena <elena.nagberi@robertsoncollege.net>
2015-04-01 13:38:42 -0500

    Class library added and LoginInfo

---
+++ TicketingSystem/ClassLibrary/LoginInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class LoginInfo
+    {
+        public int UserID { get; set; }
+        public int AccessLevelName { get; set; }
+        public string Email { get; set; }
+
+        public LoginInfo() { }
+
+        public LoginInfo(int userID, int accessLevelName, string email)
+        {
+            this.UserID = userID;
+            this.AccessLevelName = accessLevelName;
+            this.Email = email;
+        }
+    }
+}
